8d430d85899c431ab4689dd911e9f25f6022ddac
Brandon Doza <141676838+BrandonDoza@users.noreply.github.com>
2024-10-30 12:03:47 -0600

    add navlink to main page

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,11 +1,14 @@
 import './NavBar.css'
+import { NavLink } from 'react-router-dom'
 
 export default function NavBar() {
     return (
         <div className='navbar'>
-            <div className='title-element'>
-                <h1>Neighborhood Grocery</h1>
-                </div>
+         <NavLink to='/' className='title-element'>
+         <div className='title-element'>
+         <h1>Neighborhood Grocery</h1>
+         </div>
+         </NavLink>  
                 <div className='nav-buttons'>
                 <p>Search Groceries</p>
                 <p>My Cart</p>
