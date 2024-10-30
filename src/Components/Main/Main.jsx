51eb8d7c89d7c3ffebe59343c5046422b76b0e5d
Brandon Doza <141676838+BrandonDoza@users.noreply.github.com>
2024-10-30 11:24:27 -0600

    add main page component and css styling

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import './Main.css'
+
+export default function Main() {
+    return (
+        <div className='main-page'>
+            <div className='text-section'>
+            <h2 className='welcome-text'>Welcome to Neighborhood Grocery</h2>
+            <p className='about-text'>We are dedicated to bringing you the freshest, locally grown produce and farm-to-shelf products. We proudly partner with nearby farms to offer a wide selection of seasonal fruits, vegetables, and artisanal goods, all handpicked to support sustainable farming and promote healthy eating. Our shelves are stocked with organic greens, juicy berries, and hearty root vegetables that travel directly from the fields to our store, ensuring top quality and taste. At Neighborhood Grocery, we believe in building a vibrant community by supporting local farmers and delivering nutritious, fresh food you can feel good about.</p>
+            </div>
+        </div>
+        
+    )
+}
