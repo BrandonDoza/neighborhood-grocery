1487066ac819e5c8601970f20d9c0d1a969d0d8e
Brandon Doza <141676838+BrandonDoza@users.noreply.github.com>
2024-10-31 10:49:59 -0600

    add search form and state to handle user input

--- src/Components/SearchPage/Search.jsx
+++ src/Components/SearchPage/Search.jsx
@@ -1,9 +1,38 @@
 import './Search.css'
+import { useState } from 'react'
 
-export default function Search() {
+export default function Search({fetchFoodItem}) {
+    const [searchedFoods, setSearchedFoods] = useState([])
+    const [form, setForm] = useState({
+        foodItem: ''
+    })
+
+    const {foodItem} = form
+
+    function handleChange(e) {
+        setForm((prevData) => {
+            return {
+                ...prevData,
+                [e.target.name]: e.target.value
+            }
+        }
+       )
+       console.log(form)
+    }
+
     return (
         <div className='search-page'>
             <h1>This is the search area</h1>
+            <form className='add-food-form'>
+         <input className='food-input'
+         type="text"
+         placeholder='Enter an Ingredient'
+         name="foodItem"
+         value={foodItem}
+         onChange={handleChange}
+         />
+         </form>
+         <button>Submit</button>
         </div>
     )
 }
