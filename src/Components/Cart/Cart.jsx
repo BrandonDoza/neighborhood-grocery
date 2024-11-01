cb1dc0f9a4537fe1ef5cd9a352f1abb88b75f093
Brandon Doza <141676838+BrandonDoza@users.noreply.github.com>
2024-11-01 10:31:28 -0600

    display items user has added to cart in the cart component

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,7 +1,24 @@
 import './Cart.css'
+import FoodCard from '../FoodCard/FoodCard'
 
 export default function Cart({cart}) {
+
+    const itemsInCartToDisplay = cart.map((food, index) => {
+        return (
+            <FoodCard
+            key={food.id} 
+            id={food.id}
+            name={food.name}
+            img={food.img}
+            />
+        )
+    })
     return (
-        <h1>This is the cart</h1>
+        <div className='cart-display'>
+ <h1>Your Cart</h1>
+ {itemsInCartToDisplay}
+ <p>{`You have ${cart.length} item(s) in your cart`}</p>
+        </div>
+       
     )
 }
