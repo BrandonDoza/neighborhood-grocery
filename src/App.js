b5faf15a7218aca46a2a39329395a8ec43837c7d
Brandon Doza <141676838+BrandonDoza@users.noreply.github.com>
2024-11-04 13:44:24 -0700

    refactor app api calls for readability

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import './App.css'
-import Main from './Components/Main/Main'
-import NavBar from './Components/NavBar/NavBar'
-import Search from './Components/SearchPage/Search'
-import Cart from './Components/Cart/Cart'
-import Checkout from './Components/Checkout/Checkout'
-import Confirmation from './Components/Confirmation/Confirmation'
-import { Routes, Route } from 'react-router-dom'
-import { fetchFoodItem } from './apiCalls'
-import { useState } from 'react'
+import "./App.css";
+import Main from "./Components/Main/Main";
+import NavBar from "./Components/NavBar/NavBar";
+import Search from "./Components/SearchPage/Search";
+import Cart from "./Components/Cart/Cart";
+import Checkout from "./Components/Checkout/Checkout";
+import Confirmation from "./Components/Confirmation/Confirmation";
+import { Routes, Route } from "react-router-dom";
+import { fetchFoodItem } from "./apiCalls";
+import { useState } from "react";
 
 function App() {
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState([]);
 
   function addItemToCart(item) {
     setCart((prevCart) => {
-      return [...prevCart, item]
-    })
-    console.log(cart, 'cart')
+      return [...prevCart, item];
+    });
+    console.log(cart, "cart");
   }
 
   function removeItemFromCart(id) {
@@ -25,15 +25,30 @@ function App() {
 
   return (
     <div className="App">
-      <NavBar/>
+      <NavBar />
       <Routes>
-      <Route path='/' element={<Main/>}/>
-      <Route path='/search' element={<Search fetchFoodItem={fetchFoodItem} addItemToCart={addItemToCart} removeItemFromCart={removeItemFromCart} cart={cart} /> } />
-      <Route path='/cart' element={<Cart cart={cart} removeItemFromCart={removeItemFromCart}/>}/>
-      <Route path='/checkout' element={<Checkout cart={cart}/>}/>
-      <Route path='/confirmation' element={<Confirmation cart={cart} setCart={setCart}/>}/>
+        <Route path="/" element={<Main />} />
+        <Route
+          path="/search"
+          element={
+            <Search
+              fetchFoodItem={fetchFoodItem}
+              addItemToCart={addItemToCart}
+              removeItemFromCart={removeItemFromCart}
+              cart={cart}
+            />
+          }
+        />
+        <Route
+          path="/cart"
+          element={<Cart cart={cart} removeItemFromCart={removeItemFromCart} />}
+        />
+        <Route path="/checkout" element={<Checkout cart={cart} />} />
+        <Route
+          path="/confirmation"
+          element={<Confirmation cart={cart} setCart={setCart} />}
+        />
       </Routes>
-     
     </div>
   );
 }
