810f9f2dbd4320bffe269e3c266fa9bbecd9bd56
Brandon Doza <141676838+BrandonDoza@users.noreply.github.com>
2024-11-04 13:38:48 -0700

    refactor cart jsx for readability

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,34 +1,36 @@
-import './Cart.css'
-import FoodCard from '../FoodCard/FoodCard'
-import { Link } from 'react-router-dom'
+import "./Cart.css";
+import FoodCard from "../FoodCard/FoodCard";
+import { Link } from "react-router-dom";
 
-export default function Cart({cart, removeItemFromCart}) {
-
-    const itemsInCartToDisplay = cart.map((food) => {
-        return (
-            <FoodCard
-            key={food.id} 
-            id={food.id}
-            name={food.name}
-            img={food.img}
-            cart={cart}
-            removeItemFromCart={removeItemFromCart}
-            />
-        )
-    })
+export default function Cart({ cart, removeItemFromCart }) {
+  const itemsInCartToDisplay = cart.map((food) => {
     return (
-        <div className='cart-display'>
- <h1>Your Cart</h1>
- <p>{!cart.length ? 'Your Cart Is Empty' : `You have ${cart.length} item(s) in your cart`}</p>
- {itemsInCartToDisplay}
- {cart.length > 0 && (
-                <div className='button-container'>
-                    <Link to='/checkout'>
-                        <button className='checkout-button'>Checkout</button>
-                    </Link>
-                </div>
-            )}
+      <FoodCard
+        key={food.id}
+        id={food.id}
+        name={food.name}
+        img={food.img}
+        cart={cart}
+        removeItemFromCart={removeItemFromCart}
+      />
+    );
+  });
+  return (
+    <div className="cart-display">
+      <h1>Your Cart</h1>
+      <p>
+        {!cart.length
+          ? "Your Cart Is Empty"
+          : `You have ${cart.length} item(s) in your cart`}
+      </p>
+      {itemsInCartToDisplay}
+      {cart.length > 0 && (
+        <div className="button-container">
+          <Link to="/checkout">
+            <button className="checkout-button">Checkout</button>
+          </Link>
         </div>
-       
-    )
+      )}
+    </div>
+  );
 }
