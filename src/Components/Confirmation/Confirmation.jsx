e6f609a038d871b0363ca51618455924aae86582
Brandon Doza <141676838+BrandonDoza@users.noreply.github.com>
2024-11-04 13:40:33 -0700

    refactor confirmation and foodcard jsx for readability

--- src/Components/Confirmation/Confirmation.jsx
+++ src/Components/Confirmation/Confirmation.jsx
@@ -1,27 +1,28 @@
-import './Confirmation.css'
-import { Link } from 'react-router-dom'
+import "./Confirmation.css";
+import { Link } from "react-router-dom";
 
-export default function Confirmation({cart, setCart}) {
-    const itemsPurchased = cart.map((item) => {
-        return (
-            <div>
-                <h1>- {item.name}</h1>
-            </div>
-        )
-    })
-
-    function handleClick() {
-        setCart([])
-    }
+export default function Confirmation({ cart, setCart }) {
+  const itemsPurchased = cart.map((item) => {
     return (
-        <div>
-            <h1>Thank You For Your Purchase !</h1>
-            <h2>You Have Purchased The Following Item(s):</h2>
-            {itemsPurchased}
-            <Link to='/'>
-            <button className='done-button' onClick={handleClick}>Done</button>
-            </Link>
-            
-        </div>
-    )
+      <div>
+        <h1>- {item.name}</h1>
+      </div>
+    );
+  });
+
+  function handleClick() {
+    setCart([]);
+  }
+  return (
+    <div>
+      <h1>Thank You For Your Purchase !</h1>
+      <h2>You Have Purchased The Following Item(s):</h2>
+      {itemsPurchased}
+      <Link to="/">
+        <button className="done-button" onClick={handleClick}>
+          Done
+        </button>
+      </Link>
+    </div>
+  );
 }
